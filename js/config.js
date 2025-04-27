9452d3c6f628118da40c799a6de245574ac4f68c
Jonas Johansson <jns.johansson@gmail.com>
2025-04-27 14:44:59 +0200

    include funded

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ export const API_URL = "https://cobudget.com/api";
 export const GROUP_SLUG = "borderland";
 export const ROUND_SLUG = "borderland-dreams-2025";
 export const DEFAULT_LIMIT = 45;
-export const STATUS_FILTER = ["OPEN_FOR_FUNDING"];
+export const STATUS_FILTER = ["FUNDED", "OPEN_FOR_FUNDING"];
 export const DREAMS_URL =
   "https://cobudget.com/borderland/borderland-dreams-2025";
