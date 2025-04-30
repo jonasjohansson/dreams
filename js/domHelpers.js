e3f1b9ab18ebd5dc1d46593cc960b0b5ff0d1a05
Robin Nowakowski <bobinush@gmail.com>
2025-04-30 17:41:32 +0200

    Format dream description somewhat according to original style (linebreaks, bold, links etc)

--- js/domHelpers.js
+++ js/domHelpers.js
@@ -26,7 +26,17 @@ export function removeEmojis(str = "") {
 
 export function cleanCustomFieldValue(value) {
   //if (typeof value === "string" && value.trim().startsWith("<")) {
-  //  return marked.parse(value); // using marked lib
-  //}
-  return value;
+    //  return marked.parse(value); // using marked lib
+    //}
+    value = value
+    .replace(/\r\n|\r|\n/gim, '<br>') // linebreaks
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
+    .replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>') // bold text
+    .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+    .replace(/\[([^\[]+)\](\(([^)]*))\)/gim, '<a href="$3">$1</a>'); // anchor tags
+    
+
+    return value;
 }
